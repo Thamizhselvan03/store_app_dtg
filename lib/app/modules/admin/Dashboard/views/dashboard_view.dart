a99218b0255ced4791d383bc46ee908f14256c9b
doxtraxcongo@gmail.com <willsonraj91@gmail.com>
2025-06-05 19:08:10 +0530

    admin dashboard ui

--- lib/app/modules/admin/Dashboard/views/dashboard_view.dart
+++ lib/app/modules/admin/Dashboard/views/dashboard_view.dart
@@ -6,6 +6,8 @@ import 'package:store_app/app/components/common/widgets/text_app.dart';
 import '../../admin_main/views/widgets/drawer_admin.dart';
 import '../controllers/dashboard_controller.dart';
 
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
 class DashboardView extends GetView<DashboardController> {
   const DashboardView({super.key});
 
@@ -13,18 +15,105 @@ class DashboardView extends GetView<DashboardController> {
   Widget build(BuildContext context) {
     return Scaffold(
       drawer: DrawerAdmin(),
-      appBar: AppBar(title: const Text('DashboardView'), centerTitle: true),
-      body: Column(
-        children: [
-          Container(
-            height: 100.w,
-            child: Column(
+      appBar: AppBar(
+        title: const Text('DashboardView'),
+        centerTitle: true,
+        leading: Icon(Icons.menu,color: Colors.black,),
+      ),
+      body: Padding(
+        padding: EdgeInsets.symmetric(vertical: 10.h, horizontal: 15.w),
+        child: Column(
+          children: [
+            SizedBox(height: 20.h),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
               children: [
-                TextApp(text: "Total ", theme: context.textTheme.bodyMedium!),
+                CircleAvatar(
+                  child: Icon(Icons.person_2_outlined),
+                  radius: 30.r,
+                  backgroundColor: context.theme.primaryColor,
+                ),
+                CircleAvatar(
+                  child: Icon(Icons.notifications_none),
+                  radius: 30.r,
+                  backgroundColor: context.theme.primaryColor,
+                ),
+
+                CircleAvatar(
+                  child: Icon(Icons.list_alt),
+                  radius: 30.r,
+                  backgroundColor: context.theme.primaryColor,
+                ),
+
+                CircleAvatar(
+                  child: Icon(Icons.settings),
+                  radius: 30.r,
+                  backgroundColor: context.theme.primaryColor,
+                ),
               ],
             ),
-          ),
-        ],
+            SizedBox(height: 20.h),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  width: 165.w,
+
+                  height: 100.w,
+
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white,
+                  ),
+                  child: Column(
+                    children: [
+                      Spacer(),
+                      TextApp(
+                        text: "Total Racks ",
+                        theme: context.textTheme.bodyLarge!,
+                      ),
+                      Spacer(),
+                      TextApp(
+                        text: "60",
+                        theme: context.textTheme.headlineSmall!.copyWith(
+                          color: context.theme.primaryColor,
+                        ),
+                      ),
+                      Spacer(),
+                    ],
+                  ),
+                ),
+                Container(
+                  width: 165.w,
+
+                  height: 100.w,
+
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.white,
+                  ),
+                  child: Column(
+                    children: [
+                      Spacer(),
+                      TextApp(
+                        text: "Total customer ",
+                        theme: context.textTheme.bodyLarge!,
+                      ),
+                      Spacer(),
+                      TextApp(
+                        text: "150",
+                        theme: context.textTheme.headlineSmall!.copyWith(
+                          color: context.theme.primaryColor,
+                        ),
+                      ),
+                      Spacer(),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+          ],
+        ),
       ),
     );
   }
